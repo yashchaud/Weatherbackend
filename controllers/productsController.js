bad8d7adb48df28a6296c39931b8cc165509ef23
yashchaud <47110247+yashchaud@users.noreply.github.com>
2023-10-13 12:28:56 +0530

    Update productsController.js

--- controllers/productsController.js
+++ controllers/productsController.js
@@ -3,7 +3,7 @@ const axios = require('axios');
 const productsController = {
   getWeather: async (req, res, next) => {
     try {
-      const response = await axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${req.params.city}&appid=953862e849a5e99832321b555b06a2be`);
+      const response = await axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${req.params.city}&units=metric&appid=953862e849a5e99832321b555b06a2be`);
      
       res.status(200).json(response.data); // Note the .data here
     } catch (error) {
